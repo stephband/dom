26540b1ca5da0d9ccafa215f26939231d32f5757
stephband <stephband@googlemail.com>
2016-10-31 05:30:34 +0100

    Adds switxhable, toggleable. Moves bolt activate code into dom.activate.

---
+++ js/dom.switchable.js
@@ -0,0 +1,69 @@
+// dom.switchable
+//
+// Extends the default behaviour of the activate and deactivate
+// events with things to do when they are triggered on nodes.
+
+(function(window) {
+	"use strict";
+
+	var Fn      = window.Fn;
+	var dom     = window.dom;
+
+	// Define
+
+	var name = 'switchable';
+
+	// Functions
+
+	var noop            = Fn.noop;
+	var on              = dom.events.on;
+	var off             = dom.events.off;
+	var trigger         = dom.events.trigger;
+
+	function jump(e) {
+		// A prevented default means this link has already been handled.
+		if (e.defaultPrevented) { return; }
+
+		// e.data is the pane to jump to.
+		trigger(e.data, 'activate');
+		e.preventDefault();
+	}
+
+	function activate(e) {
+		if (!e.default) { return; }
+
+		var target = e.target;
+		if (!dom.classes(target).contains(name)) { return; }
+
+		var data  = e.data;
+		var nodes = dom('.switchable', target.parentNode).toArray();
+		var i     = nodes.indexOf(target);
+
+		on(target, 'click',  jump, nodes[Fn.wrap(0, nodes.length, i - 1)], 'a[href="#prev"]');
+		on(target, 'click',  jump, nodes[Fn.wrap(0, nodes.length, i + 1)], 'a[href="#next"]');
+
+		nodes.splice(i, 1);
+		var active = nodes.filter(dom.matches('.active'));
+
+		e.default();
+
+		// Deactivate the previous active pane AFTER this pane has been
+		// activated. It's important for panes who's style depends on the
+		// current active pane, eg: .slide.active ~ .slide
+		Fn(active).each(dom.trigger('deactivate'));
+	}
+
+	function deactivate(e) {
+		if (!e.default) { return; }
+
+		var target = e.target;
+		if (!dom.classes(target).contains(name)) { return; }
+
+		off(e.target, 'click swiperight swipeleft', jump);
+		e.default();
+	}
+
+	on(document, 'activate', activate);
+	on(document, 'deactivate', deactivate);
+
+})(this);
---
+++ js/dom.toggleable.js
@@ -0,0 +1,70 @@
+// dom.toggleable
+
+(function(window) {
+	"use strict";
+
+	var Fn      = window.Fn;
+	var dom     = window.dom;
+
+	// Define
+
+	var name = 'toggleable';
+
+	// Functions
+
+	var noop            = Fn.noop;
+	var on              = dom.events.on;
+	var off             = dom.events.off;
+	var trigger         = dom.events.trigger;
+
+	function click(e, activeTarget) {
+		// A prevented default means this link has already been handled.
+		if (e.defaultPrevented) { return; }
+		if (!dom.isPrimaryButton(e)) { return; }
+
+		var node = e.currentTarget;
+		if (!node) { return; }
+
+		trigger(activeTarget, 'deactivate', {
+			relatedTarget: e.target
+		});
+
+		e.preventDefault();
+	}
+
+	function activate(e) {
+		if (!e.default) { return; }
+
+		var target = e.target;
+		if (!dom.classes(target).contains(name)) { return; }
+
+		var id = dom.identify(target);
+
+		dom('[href$="#' + id + '"]')
+		.each(function(node) {
+			on(node, 'click', click, e.target);
+		});
+
+		e.default();
+	}
+
+	function deactivate(e, data, fn) {
+		if (!e.default) { return; }
+
+		var target = e.target;
+		if (!dom.classes(target).contains(name)) { return; }
+
+		var id = dom.identify(e.target);
+
+		dom('[href$="#' + id + '"]')
+		.each(function(node) {
+			off(node, 'click', click);
+		});
+
+		e.default();
+	}
+
+	on(document, 'activate', activate);
+	on(document, 'deactivate', deactivate);
+
+})(this);

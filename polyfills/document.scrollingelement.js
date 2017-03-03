1c2641c57f3fa8f5f85ebcc1b17989d9c8178131
stephband <stephband@googlemail.com>
2017-03-03 02:00:23 +0100

    Adds document.scrollingElement polyfill

---
+++ polyfills/document.scrollingelement.js
@@ -0,0 +1,41 @@
+/*! https://mths.be/scrollingelement v1.5.2 by @diegoperini & @mathias | MIT license */
+if (!('scrollingElement' in document)) (function() {
+	// Note: standards mode / quirks mode can be toggled at runtime via
+	// `document.write`.
+	var isCompliantCached;
+
+	function isCompliant() {
+		var isStandardsMode = /^CSS1/.test(document.compatMode);
+		if (!isStandardsMode) {
+			// In quirks mode, the result is equivalent to the non-compliant
+			// standards mode behavior.
+			return false;
+		}
+		if (isCompliantCached === void 0) {
+			// When called for the first time, check whether the browser is
+			// standard-compliant, and cache the result.
+			var iframe = document.createElement('iframe');
+			iframe.style.height = '1px';
+			(document.body || document.documentElement || document).appendChild(iframe);
+			var doc = iframe.contentWindow.document;
+			doc.write('<!DOCTYPE html><div style="height:9999em">x</div>');
+			doc.close();
+			isCompliantCached = doc.documentElement.scrollHeight > doc.body.scrollHeight;
+			iframe.parentNode.removeChild(iframe);
+		}
+		return isCompliantCached;
+	}
+
+	function scrollingElement() {
+		return isCompliant() ? document.documentElement : document.body ;
+	}
+
+	if (Object.defineProperty) {
+		// Support modern browsers that lack a native implementation.
+		Object.defineProperty(document, 'scrollingElement', {
+			'get': scrollingElement
+		});
+	} else {
+		document.scrollingElement = scrollingElement();
+	}
+}());

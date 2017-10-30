9ca2f99a9c01cb961f53f99a86ffc83a99319eff
stephband <stephband@googlemail.com>
2017-10-30 16:30:03 +0100

    Adds analyser

---
+++ js/dom.analyse.js
@@ -0,0 +1,32 @@
+(function(window) {
+    "use strict";
+
+    var Fn      = window.Fn;
+    var dom     = window.dom;
+
+    var rspaces = Fn.rspaces;
+
+    function analyse(category, action, label, value) {
+        window.ga && window.ga('send', 'event', category, action, label, value);
+    }
+
+    function analyseTime(category, action, label, time) {
+        // Time should be an integer, in milliseconds
+        time = Math.round(time || window.performance.now());
+        window.ga && window.ga('send', 'timing', category, action, time, label);
+    }
+
+    function dashesToSpaces(string) {
+        return string.replace('-', ' ');
+    }
+
+    dom
+    .event('change', document)
+    .map(Fn.get('target'))
+    .map(dom.closest('[data-analyse]'))
+    .each(function(node) {
+        var property = dom.attribute('data-analyse', node);
+        var labels   = property.split(rspaces).map(dashesToSpaces);
+        analyse.apply(null, labels);
+    });
+})(this);

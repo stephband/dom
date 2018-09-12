50a8ed8bdb447c869143cda06435e5f306c0c742
stephband <stephband@googlemail.com>
2018-09-12 16:17:48 +0200

    Todo

--- modules/children.js
+++ modules/children.js
@@ -3,5 +3,7 @@ import { toArray } from '../../fn/fn.js';
 export default function children(node) {
 	// In IE and Safari, document fragments do not have .children, fall back to
 	// querySelectorAll.
+
+	// TOIDO: BUg in selector!!!
 	return toArray(node.children || node.querySelectorAll('*'));
 }

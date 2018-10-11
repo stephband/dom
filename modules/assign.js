f931af94686adf03704da4c0a74681c1a51105c3
stephband <stephband@googlemail.com>
2018-10-11 19:41:47 +0200

    assign takes attributes html (innerHTML) children (array)

--- modules/assign.js
+++ modules/assign.js
@@ -1,13 +1,30 @@
+import { id, overload } from '../../fn/fn.js';
+import append from './append.js';
+
+const setAttribute = overload(id, {
+	html: function(name, node, content) {
+		node.innerHTML = content;
+	},
+
+	children: function(name, node, content) {
+		content.forEach((child) => { node.appendChild(child) });
+	},
+
+	default: function(name, node, content) {
+		if (name in node) {
+			node[name] = content;
+		}
+		else {
+			node.setAttribute(name, content);
+		}
+	}
+});
+
 export default function assignAttributes(node, attributes) {
 	var names = Object.keys(attributes);
 	var n = names.length;
 
 	while (n--) {
-		if (names[n] in node) {
-			node[names[n]] = attributes[names[n]];
-		}
-		else {
-			node.setAttribute(names[n], attributes[names[n]]);
-		}
+		setAttribute(names[n], node, attributes[names[n]]);
 	}
 }

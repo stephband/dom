85cdfc9b7643882ff1091c4e820a5cbd9c2cb477
stephband <stephband@googlemail.com>
2019-05-16 19:47:29 +0200

    Support BPM units in min max tick attributes

--- components/control/control.js
+++ components/control/control.js
@@ -174,13 +174,17 @@ export function evaluate(string) {
     const number = +string;
     if (number || number === 0) { return number; }
 
-    const tokens = /^(-?[\d.]+)(?:(dB)|(m|k)?(\w+))$/.exec(string);
+    const tokens = /^(-?[\d.]+)(?:(dB|bpm)|(m|k)?(\w+))$/.exec(string);
     if (!tokens) { return 0 };
 
     const value = parseFloat(tokens[1]) ;
 
     return tokens[2] === 'dB' ? toLevel(value) :
+        // BPM to rate in beats per second
+        tokens[2] === 'bpm' ? value / 60 :
+        // milli-
         tokens[3] === 'm' ? value / 1000 :
+        // kilo-
         tokens[3] === 'k' ? value * 1000 :
         value ;
 }

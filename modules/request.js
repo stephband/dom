c7053f7fc783b5ed10df3ba49073f4a1ec657947
stephband <stephband@googlemail.com>
2019-12-02 20:36:47 +0100

    Request cancelling

--- modules/request.js
+++ modules/request.js
@@ -132,18 +132,18 @@ function serialize(formData) {
 	return new URLSearchParams(formData).toString();
 }
 
-function createOptions(method, mimetype, data, signal) {
+function createOptions(method, mimetype, data, controller) {
 	return method === 'GET' ? {
 		method:  method,
 		headers: createHeaders(mimetype, data),
 		credentials: 'same-origin',
-        signal: signal
+        signal: controller && controller.signal
 	} : {
 		method:  method,
 		headers: createHeaders(mimetype, data),
 		body:    createBody(mimetype, data),
 		credentials: 'same-origin',
-        signal: signal
+        signal: controller && controller.signal
 	} ;
 }
 
@@ -180,24 +180,21 @@ function respond(response) {
     return responders[mimetype](response);
 }
 
+
 /*
 request(type, mimetype, url, data)
 */
 
 export default function request(type = 'GET', mimetype = 'application/json', url, data) {
-	const method     = type.toUpperCase();
-    const controller = new AbortController();
+	const method = type.toUpperCase();
 
-	return assign(fetch(url, createOptions(method, mimetype, data, controller.signal))
-    .then(respond), {
-        cancel: function() {
-            controller.abort();
-        }
-    });
+    // param[4] is an optional abort controller
+	return fetch(url, createOptions(method, mimetype, data, arguments[4]))
+    .then(respond);
 }
 
 export function requestGet(url) {
-	return request('GET', 'application/json', url);
+	return request('GET', 'application/json', url, {});
 }
 
 export function requestPatch(url, data) {
@@ -211,3 +208,48 @@ export function requestPost(url, data) {
 export function requestDelete(url, data) {
 	return request('DELETE', 'application/json', url, data);
 }
+
+
+/*
+throttledRequest(type, mimetype, url)
+*/
+
+function ignoreAbortError(error) {
+    // Swallow AbortErrors, since we generate one every time we use
+    // the AbortController.
+    if (error.name === 'AbortError') {
+        console.log('Request aborted deliberately, swollowing error');
+
+        // JS promises have no machanism to conditionally catch different
+        // types of error – throw undefined to fall through to the next
+        // catch without a value.
+        throw undefined;
+    }
+
+    // Rethrow all other errors
+    throw error;
+}
+
+export function throttledRequest(type, mimetype, url) {
+    var promise, controller;
+
+    return function throttle(data) {
+        var p;
+
+        if (promise) {
+            // Cancel previous request
+            controller.abort();
+        }
+
+        controller = new AbortController();
+
+        return promise = p = request(type, mimetype, url, data, controller)
+        .finally(() => {
+            // Promise may not be the same promise by the time we get here
+            if (promise !== p) { return; }
+            promise    = undefined ;
+            controller = undefined ;
+        })
+        .catch(ignoreAbortError);
+    };
+};

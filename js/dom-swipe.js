081f1390eeeb670e27c00dfe995f5f503ae7010b
stephband <stephband@googlemail.com>
2019-12-03 17:18:54 +0100

    Format

--- js/dom-swipe.js
+++ js/dom-swipe.js
@@ -5,14 +5,14 @@ import './dom-gesture.js';
 const on      = events.on;
 const trigger = events.trigger;
 
-//	var settings = {
-//		// Ratio of distance over target finger must travel to be
-//		// considered a swipe.
-//		threshold: 0.4,
-//		// Faster fingers can travel shorter distances to be considered
-//		// swipes. 'sensitivity' controls how much. Bigger is shorter.
-//		sensitivity: 6
-//	};
+//    var settings = {
+//        // Ratio of distance over target finger must travel to be
+//        // considered a swipe.
+//        threshold: 0.4,
+//        // Faster fingers can travel shorter distances to be considered
+//        // swipes. 'sensitivity' controls how much. Bigger is shorter.
+//        sensitivity: 6
+//    };
 
 function touchdone(node, data, x0, y0, t0) {
 	//var x = data.x;
@@ -39,8 +39,8 @@ on(document, 'dom-gesture', function(e) {
 
 	var touch = e.detail();
 
-    touch
-    .clone()
-    .reduce(argument(1))
-    .then((data) => touchdone(node, data, e.pageX, e.pageY, e.timeStamp));
+	touch
+	.clone()
+	.reduce(argument(1))
+	.then((data) => touchdone(node, data, e.pageX, e.pageY, e.timeStamp));
 });

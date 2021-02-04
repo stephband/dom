db9d7c61aa847761d4b41153e500f5b8dfe67a25
stephband <stephband@googlemail.com>
2021-02-04 15:22:47 +0100

    delegate parameters

--- modules/delegate.js
+++ modules/delegate.js
@@ -1,4 +1,19 @@
 
+/** 
+delegate(map)
+
+Takes an object map of functions keyed to selectors, and returns a function that
+handles event objects, delegating them to any functions whose selector matches 
+the event target. Functions are passed the target node and the event object plus 
+any other arguments passed to the handler (allowing delegate to be used as a reducer).
+
+```
+delegate({
+	'button': (button, event) => {}
+})
+```
+**/
+
 export default function delegate(object) {
 	if (typeof object !== 'object' || arguments.length > 1) {
 		throw new Error('delegate() now takes an object of selector:fn pairs.');
@@ -11,13 +26,11 @@ export default function delegate(object) {
 	return function handle(e) {
 		const target = e.target;
 		let n = -1;
+
 		while (selectors[++n]) {
 			const node = target.closest(selectors[n]);
 			if (node) {
-				// Todo: remove the need for delegateTarget in Bolt dom-activate
-				e.delegateTarget = node;
-				object[selectors[n]](e, node);
-				e.delegateTarget = undefined;
+				return object[selectors[n]](node, ...arguments);
 			}
 		}
 	};

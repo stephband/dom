af6e3019e01015e3cdb94d45613b9b4c87ea87c0
stephband <stephband@googlemail.com>
2021-02-26 09:58:22 +0100

    Scroll behavior

--- polyfills/element.scrollintoview.js
+++ polyfills/element.scrollintoview.js
@@ -10,7 +10,8 @@ import noop     from '../../fn/modules/noop.js';
 import animate  from '../modules/animate.js';
 import rect     from '../modules/rect.js';
 import features from '../modules/features.js';
-import offset   from '../modules/offset.js';
+
+const DEBUG = true;
 
 // Duration and easing of scroll animation
 const config = {
@@ -19,72 +20,83 @@ const config = {
     scrollTransform: expOut(3)
 };
 
-let cancel = noop;
-
-function scrollToNode(target, scrollParent, behavior) {
-    const style             = getComputedStyle(scrollParent);
-    const scrollPaddingLeft = parseInt(style.scrollPaddingLeft, 10);
-    const scrollPaddingTop  = parseInt(style.scrollPaddingTop, 10);
-    const coords            = offset(scrollParent, target);
-    const scrollWidth       = scrollParent.scrollWidth;
-    const scrollHeight      = scrollParent.scrollHeight;
-
-    const scrollBoxWidth = scrollParent === document.body ?
-        // We cannot guarantee that body height is 100%. Use the window
-        // innerHeight instead.
-        window.innerWidth :
-        rect(scrollParent).width ;
-
-    const scrollBoxHeight = scrollParent === document.body ?
-        // We cannot guarantee that body height is 100%. Use the window
-        // innerHeight instead.
-        window.innerHeight :
-        rect(scrollParent).height ;
-
-    const left = (coords[0] - scrollPaddingLeft) > (scrollWidth - scrollBoxWidth) ?
-            scrollWidth - scrollBoxWidth :
-            (scrollParent.scrollLeft + coords[0] - scrollPaddingLeft) ;
-
-    const top = (coords[1] - scrollPaddingTop) > (scrollHeight - scrollBoxHeight) ?
-        scrollHeight - scrollBoxHeight :
-        (scrollParent.scrollTop + coords[1] - scrollPaddingTop) ;
-
-    cancel();
-
-    const scrollLeft = left < 0 ?
-        0 :
-    left > scrollWidth - scrollBoxWidth ?
-        scrollWidth - scrollBoxWidth :
-    left ;
-
-    const scrollTop = top < 0 ?
-        0 :
-    top > scrollHeight - scrollBoxHeight ?
-        scrollHeight - scrollBoxHeight :
-    top ;
-
-    if (behavior === 'smooth') {
-        //const scrollDuration = config.scrollDuration
-        //    + config.scrollDurationPerHeight
-        //    * Math.abs(scrollTop - scrollParent.scrollTop)
-        //    / scrollBoxHeight ;
-
-        cancel = animate(0.3, config.scrollTransform, 'scrollLeft', scrollParent, scrollLeft);
-        //cancel = animate(scrollDuration, config.scrollTransform, 'scrollTop', scrollParent, scrollTop);
-    }
-    else {
-        scrollParent.scrollLeft = scrollLeft ;
-        scrollParent.scrollTop  = scrollTop ;
-    }
-}
 
 if (!features.scrollBehavior) {
     console.log('Polyfilling Element.scrollIntoView(options).');
+
+    let cancel = noop;
 
     // Get the method from HTMLElement - in some browsers it is here rather
     // than on Element
     const constructor    = 'scrollIntoView' in Element.prototype ? Element : HTMLElement ;
     const scrollIntoView = constructor.scrollIntoView;
+    const scrollToNode   = function scrollToNode(target, scrollParent, behavior) {
+        // If element does not scroll with content
+        if (getComputedStyle(target).position === 'fixed') {
+            if (DEBUG) { console.log('Target is position: fixed, cannot scroll to.') }
+            return;
+        }
+
+        const targetBox         = rect(target);
+        const style             = getComputedStyle(scrollParent);
+        const scrollPaddingLeft = parseInt(style.scrollPaddingLeft, 10);
+        const scrollPaddingTop  = parseInt(style.scrollPaddingTop, 10);
+
+        const parentBox       = rect(scrollParent);      
+        const coords          = [targetBox.left - parentBox.left, targetBox.top - parentBox.top];
+        const scrollWidth     = scrollParent.scrollWidth;
+        const scrollHeight    = scrollParent.scrollHeight;
+
+        const scrollBoxWidth  = scrollParent === document.body ?
+            // We cannot guarantee that body height is 100%. Use the window
+            // innerHeight instead.
+            window.innerWidth :
+            parentBox.width ;
+
+        const scrollBoxHeight = scrollParent === document.body ?
+            // We cannot guarantee that body height is 100%. Use the window
+            // innerHeight instead.
+            window.innerHeight :
+            parentBox.height ;
+
+        const left = (coords[0] - scrollPaddingLeft) > (scrollWidth - scrollBoxWidth) ?
+                scrollWidth - scrollBoxWidth :
+                (scrollParent.scrollLeft + coords[0] - scrollPaddingLeft) ;
+
+        const top = (coords[1] - scrollPaddingTop) > (scrollHeight - scrollBoxHeight) ?
+            scrollHeight - scrollBoxHeight :
+            (scrollParent.scrollTop + coords[1] - scrollPaddingTop) ;
+
+        cancel();
+
+        const scrollLeft = left < 0 ?
+            0 :
+        left > scrollWidth - scrollBoxWidth ?
+            scrollWidth - scrollBoxWidth :
+        left ;
+
+        const scrollTop = top < 0 ?
+            0 :
+        top > scrollHeight - scrollBoxHeight ?
+            scrollHeight - scrollBoxHeight :
+        top ;
+console.log(behavior);
+        if (behavior === 'smooth') {
+            console.log('ANIMATE', config.scrollTransform, 'scrollTop', scrollParent, scrollTop);
+
+            //const scrollDuration = config.scrollDuration
+            //    + config.scrollDurationPerHeight
+            //    * Math.abs(scrollTop - scrollParent.scrollTop)
+            //    / scrollBoxHeight ;
+    
+            //cancel = animate(0.3, config.scrollTransform, 'scrollLeft', scrollParent, scrollLeft);
+            cancel = animate(0.3, config.scrollTransform, 'scrollTop', scrollParent, scrollTop);
+        }
+        else {
+            scrollParent.scrollLeft = scrollLeft ;
+            scrollParent.scrollTop  = scrollTop ;
+        }
+    };
 
     constructor.prototype.scrollIntoView = function(options) {
         if (typeof options === 'object') {
@@ -108,7 +120,7 @@ if (!features.scrollBehavior) {
                 scrollParent = this;
     
                 while((scrollParent = scrollParent.parentNode)) {
-                    if (scrollParent === document.body || scrollParent === document.documentElement) {
+                    if (scrollParent === document.body || scrollParent === document.documentElement || scrollParent === document || scrollParent === window) {
                         scrollParent = document.scrollingElement;
                         break;
                     }
@@ -119,7 +131,7 @@ if (!features.scrollBehavior) {
                     }
                 }
             }
-
+            
             scrollToNode(this, scrollParent, options.behavior);
         }
         else {
---
+++ scripts/scroll-behavior.js
@@ -0,0 +1,143 @@
+
+import '../polyfills/document.scrollingelement.js';
+//import '../polyfills/element.scrollintoview.js';
+
+import cache    from '../../fn/modules/weak-cache.js';
+import features from '../../dom/modules/features.js';
+import rect     from '../../dom/modules/rect.js';
+
+const DEBUG = true;
+
+/* 
+Smooth scroll for browsers that do not have it
+*/
+
+if (!features.scrollBehavior) {
+    const scrollOptions = {
+        behavior: 'auto',
+        block: 'start'
+    };
+
+    const store = cache(function store(node) {
+        return {
+            node: node
+        };
+    });
+
+    const restoreScroll = function restoreScroll(element) {
+        while (element = element.parentNode) {
+            if (element === document.body || element === document.documentElement) {
+                element = document.scrollingElement;
+                break;
+            }
+    
+            if (element.scrollHeight > element.clientHeight) {
+                break;
+            }
+        }
+
+        var data = store(element);
+        element.scrollLeft = data.scrollLeft;
+        element.scrollTop  = data.scrollTop;
+    }
+
+    // Keep a record of scrollTop in order to restore it
+    window.addEventListener('scroll', function scroll(e) {
+        const element = (e.target === document.body
+            || e.target === document.documentElement 
+            || e.target === document 
+            || e.target === window) ?
+                document.scrollingElement :
+                e.target;
+
+        const data = store(element);
+        data.time       = e.timeStamp / 1000;
+        data.scrollLeft = element.scrollLeft;
+        data.scrollTop  = element.scrollTop;
+    }, {
+        capture: true,
+        passive: true
+    });
+
+    window.addEventListener('hashchange', function hashchange(e) {        
+        const id = window.location.hash.replace(/^#/, '');
+        if (!id) { return; }
+
+        const target = document.getElementById(id);
+        if (!target) { return; }
+
+        // If element does not scroll with content do not try to scroll to it
+        if (getComputedStyle(target).position === 'fixed') {
+            if (DEBUG) { console.log('hashchange: Target is position: fixed, do not attempt scroll to.') }
+            return;
+        }
+
+        const element = (target.parentNode === document.body
+            || target.parentNode === document.documentElement 
+            || target.parentNode === document 
+            || target.parentNode === window) ?
+                document.scrollingElement :
+                target.parentNode;
+
+        const time = e.timeStamp / 1000;
+        const data = store(element);
+
+        if (data.time > time - 0.3) {
+            if (DEBUG) { console.log('hashchange: Scrolling was recent, do not attempt scroll to'); }
+            return;
+        }
+
+        const targetBox         = rect(target);
+        const style             = getComputedStyle(element);
+        const scrollPaddingLeft = parseInt(style.scrollPaddingLeft, 10);
+        const scrollPaddingTop  = parseInt(style.scrollPaddingTop, 10);
+
+        /*
+        console.log('IN VIEWPORT',
+            targetBox.top >= scrollPaddingTop,
+            targetBox.top < 0.8 * window.innerHeight,
+            targetBox.left >= scrollPaddingLeft,
+            targetBox.left < 0.8 * window.innerWidth
+        );
+        */
+
+        // If target is already substantially on screen do nothing
+        if (targetBox.top >= scrollPaddingTop
+            && targetBox.top < 0.8 * window.innerHeight
+            && targetBox.left >= scrollPaddingLeft
+            && targetBox.left < 0.8 * window.innerWidth
+        ) {
+            if (DEBUG) { console.log('hashchange: Target is already substantially inside the viewport, do not attempt scroll to') }
+            return;
+        }
+
+        console.log('hashchange', window.location.hash.replace(/^#/, ''));
+
+        // In Safari, hashchange is preceeded by scroll jump - restore 
+        // previous scrollTop.
+        //restoreScroll(target);
+
+        // Then animate
+        //requestAnimationFrame(function() {
+            //console.log('SCROLL INTO VIEW', target.id);
+            //target.scrollIntoView(scrollOptions);
+        //});
+    });
+}
+else {
+    // Override initial smooth scroll with an instant scroll to target
+    window.addEventListener('load', function load(e) {
+        const target = document.getElementById(window.location.hash.replace(/^#/, ''));
+        if (!target) { return; }
+
+        target.scrollIntoView({
+            behavior: 'auto',
+            block: 'start'
+        });
+    });
+    
+
+    window.addEventListener('hashchange', function() {
+        console.log('hashchange', window.location.hash);
+    });
+}

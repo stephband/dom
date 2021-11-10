782e26954774b83650d96ae76c628eb38418dae2
stephband <stephband@googlemail.com>
2021-11-10 18:23:41 +0100

    Adds frames stream

---
+++ modules/frames.js
@@ -0,0 +1,21 @@
+
+import Stream from '../../fn/stream/stream.js';
+
+export function frames() {
+    var timer;
+    return new Stream((control) => {
+        function frame(t) {
+            timer = window.requestAnimationFrame(frame);
+            control.push(t);
+        }
+
+        timer = window.requestAnimationFrame(frame);
+
+        control.done({
+            stop: function() {
+                timer && cancelAnimationFrame(timer);
+                timer = undefined;
+            }
+        });
+    });
+}

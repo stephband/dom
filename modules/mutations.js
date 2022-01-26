02baf073f1be5e797d2c0c0182930aad721ec37e
Stephen <stephband@cruncher.ch>
2022-01-26 12:18:31 +0100

    Adds mutations()

---
+++ modules/mutations.js
@@ -0,0 +1,73 @@
+
+/**
+mutations(type, element)
+Creates a stream of mutations (powered by a `MutationObserver`), where `type` is
+one of `'all'`, `'attributes'`, `'children'`, `'tree'`, `'text'`, or an object
+of options for the `MutationObserver`.
+**/
+
+import Stream, { Source } from '../../fn/stream/stream.js';
+
+
+/*
+Types of `options` shortcuts
+*/
+
+const types = {
+    all: {
+        attributes:    true,
+        characterData: true,
+        childList:     true,
+        subtree:       true
+    },
+
+    attributes: {
+        attributes: true
+    },
+
+    children: {
+        childList: true
+    },
+
+    tree: {
+        childList: true,
+        subtree: true
+    },
+
+    text: {
+        characterData: true
+    }
+};
+
+/*
+MutationSource(element, options)
+*/
+
+function MutationSource(element, options) {
+    this.element = element;
+    this.options = options;
+}
+
+assign(MutationSource.prototype, Source.prototype, {
+    /* Inherited from Source .pipe(), .done() */
+
+    start: function(stream) {
+        const options = typeof type === 'string' ? types[type] : type ;
+        this.observer = new MutationObserver((mutations) => this.target.push(mutations));
+        this.observer.observe(element, options);
+    },
+
+    stop: function() {
+        this.observer.disconnect();
+        Source.prototype.stop.apply(this);
+    }
+});
+
+/*
+mutations(type, element)
+*/
+
+export function mutations(type, element) {
+    const options = typeof type === 'string' ? types[type] : type ;
+    return new Stream(new MutationSource(element, options));
+}

7b8ce5590f55297360d054e7014fc1dad2e488a5
Stephen <stephband@cruncher.ch>
2023-04-20 16:20:43 +0200

    isWebkit

--- modules/is-webkit.js
+++ modules/is-webkit.js
@@ -1,5 +1,6 @@
-var isWebkit  = /Webkit/i.test(navigator.userAgent);
-var isChrome  = /Chrome/i.test(navigator.userAgent);
-var isiOS     = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+const isWebkit  = /Webkit/i.test(navigator.userAgent);
+const isChrome  = /Chrome/i.test(navigator.userAgent);
+const isiOS     = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
 export default isWebkit && (isiOS || !isChrome);

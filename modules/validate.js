ee68f886bdfdd9daab9ae9302a0dec38330f1065
Stephen <stephband@cruncher.ch>
2023-08-18 14:35:34 +0200

    Adds validate

--- modules/validate.js
+++ modules/validate.js
@@ -1,4 +1,16 @@
 
+/**
+validate(element)
+Validates an element using `.checkValidity()` on that element, or by finding and
+running checkValidity on the element's descendents.
+**/
+
+// TODO: Need a better way of finding validateable elements, which may now
+// include any form-enabled custom element
+const selector = 'input, select, textarea';
+
 export default function validate(node) {
-    return node.checkValidity ? node.checkValidity() : true ;
+    return node.checkValidity ? node.checkValidity() :
+		node.hasChildNodes() ? Array.from(node.querySelectorAll(selector)).every(validate) :
+		true ;
 }

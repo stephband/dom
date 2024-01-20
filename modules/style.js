4d851e9acfa24cdf96ac4804049d0f30f3dc5f66
Stephen <stephband@cruncher.ch>
2024-01-20 08:42:55 +0100

    parseAngle

--- modules/style.js
+++ modules/style.js
@@ -2,56 +2,13 @@
 style(property, node)
 
 Returns the computed style `property` of `node`.
-
-    style('transform', node);            // returns transform
-
-If `property` is of the form `"property:name"`, a named aspect of the property
-is returned.
-
-    style('transform:rotate', node);     // returns rotation, as a number, in radians
-    style('transform:scale', node);      // returns scale, as a number
-    style('transform:translateX', node); // returns translation, as a number, in px
-    style('transform:translateY', node); // returns translation, as a number, in px
+    style('transform', node);  // returns transform
 */
 
-var rpx          = /px$/;
-var styleParsers = {
-	"transform:translateX": function(node) {
-		var matrix = computedStyle('transform', node);
-		if (!matrix || matrix === "none") { return 0; }
-		var values = valuesFromCssFn(matrix);
-		return parseFloat(values[4]);
-	},
+import parseAngle from '../../fn/modules/parse-angle.js';
 
-	"transform:translateY": function(node) {
-		var matrix = computedStyle('transform', node);
-		if (!matrix || matrix === "none") { return 0; }
-		var values = valuesFromCssFn(matrix);
-		return parseFloat(values[5]);
-	},
-
-	"transform:scale": function(node) {
-		var matrix = computedStyle('transform', node);
-		if (!matrix || matrix === "none") { return 0; }
-		var values = valuesFromCssFn(matrix);
-		var a = parseFloat(values[0]);
-		var b = parseFloat(values[1]);
-		return Math.sqrt(a * a + b * b);
-	},
-
-	"transform:rotate": function(node) {
-		var matrix = computedStyle('transform', node);
-		if (!matrix || matrix === "none") { return 0; }
-		var values = valuesFromCssFn(matrix);
-		var a = parseFloat(values[0]);
-		var b = parseFloat(values[1]);
-		return Math.atan2(b, a);
-	}
-};
-
-function valuesFromCssFn(string) {
-	return string.split('(')[1].split(')')[0].split(/\s*,\s*/);
-}
+var rpx    = /px$/;
+var rangle = /deg$|turn$|rad$/;
 
 function computedStyle(name, node) {
 	return window.getComputedStyle ?
@@ -62,18 +19,12 @@ function computedStyle(name, node) {
 }
 
 export default function style(name, node) {
-    // If name corresponds to a custom property name in styleParsers...
-    if (styleParsers[name]) { return styleParsers[name](node); }
-
     var value = computedStyle(name, node);
 
     // Pixel values are converted to number type
-    return typeof value === 'string' && rpx.test(value) ?
-        parseFloat(value) :
-        value ;
-}
-
-// Expose to console in DEBUG mode
-if (window.DEBUG) {
-    Object.assign(window.dom || (window.dom = {}), { style });
+    return typeof value === 'string' ?
+		rpx.test(value) ? parseFloat(value) :
+		rangle.test(value) ? parseAngle(value) :
+        value :
+		value ;
 }

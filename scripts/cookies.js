ed750b5f234de03e216d8330d2b2b228baedb768
Stephen <stephband@cruncher.ch>
2024-07-29 14:18:42 +0200

    Export

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -23,7 +23,7 @@ Syntaxes:
 
 */
 
-window.cookies = {
+export default {
 	get: function (sKey) {
 		if (!sKey) { return null; }
 		return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;

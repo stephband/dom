baca7d6e10237a9c0f79fd6aa7559f6bb62214c3
Stephen <stephband@cruncher.ch>
2024-10-17 17:25:00 +0200

    createNumberProperty

--- modules/element/create-number-property.js
+++ modules/element/create-number-property.js
@@ -2,7 +2,7 @@
 import clamp  from 'fn/clamp.js';
 import Signal from 'fn/signal.js';
 
-export default function createNumberProperty(min = -Infinity, max = Infinity, initial = clamp(min, max, 0)) {
+export default function createNumberProperty(min = -Infinity, max = Infinity, parse = Number, initial = clamp(min, max, 0)) {
     const symbol = Symbol();
     const descriptor = {
         attribute: function(value) {
@@ -15,8 +15,8 @@ export default function createNumberProperty(min = -Infinity, max = Infinity, in
         },
 
         set: function(value) {
-            const signal = this[symbol] || (this[symbol] = Signal.of(value));
-            const number = Number(value);
+            const signal = this[symbol] || (this[symbol] = Signal.of());
+            const number = parse(value);
 
             if (Number.isNaN(number)) {
                 throw new TypeError('Attempt to set ' + typeof value + ' on property, expects a number');
